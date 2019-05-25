7418dee4efa11d958212982ea1342ec99f0bcbd3
Sangin <sangin.kang@navercorp.com>
2019-05-25 22:37:52 +0900

    Working context

---
+++ src/PageContent.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class PageContent extends Component {
+  render() {
+    const styles = {
+      backgroundColor: "red",
+      height: "100vh",
+      width: "100vh"
+    };
+    return <div />;
+  }
+}
